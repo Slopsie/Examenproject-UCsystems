84cbbb2028e146e31e901fab754a7f56a852b767
Ralph V <Ralphverspui@live.nl>
2018-05-22 13:51:26 +0200

    Login en registreren pagina toegevoegd
    + buttons op de homepage

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,4 +1,4 @@
 <h2><?= $title ?></h2>
 <p>Welcome to the ciBlog application.</p>
-<button type="button" class="btn btn-outline-primary">Login</button>
-<button type="button" class="btn btn-outline-primary">Registreren</button>
+<button type="button" class="btn btn-outline-info">Login</button>
+<button type="button" class="btn btn-outline-info">Registreren</button>
---
+++ application/views/pages/login.php
@@ -0,0 +1,7 @@
+<h2><?= $title ?></h2>
+<p>This is ciBlog version 1.0.</p>
+<form>
+	<input type="text" name="username" placeholder="Gebruikersnaam">
+	<input type="text" name="password" placeholder="Wachtwoord">
+	<button type="button" class="btn btn-outline-info">Login</button>
+</form>
