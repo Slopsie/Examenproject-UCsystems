188edf75d961b0175fd3426b311817595da043e4
Ralph V <Ralphverspui@live.nl>
2018-05-22 11:49:30 +0200

    Buttons Toegevoegd op Home

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,2 +1,4 @@
 <h2><?= $title ?></h2>
 <p>Welcome to the ciBlog application.</p>
+<button type="button" class="btn btn-outline-primary">Login</button>
+<button type="button" class="btn btn-outline-primary">Registreren</button>
