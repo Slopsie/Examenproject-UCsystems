d15a427bb16e19f3cea7098ad9a6580e9d1a7c33
Ralph V <Ralphverspui@live.nl>
2018-05-23 11:51:21 +0200

    Bug Fix; Login en registreren in menu opmaakt gefixed
    Registreren en Login vielen na vorige commit buiten de opmaak van de rest van de header.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -28,13 +28,13 @@
     <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>tickets">Tickets</a>
       </li>
-    </ul>
     <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>login">Login</a>
       </li>
     <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>registreren">Registreren</a>
       </li>
+  </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search">
       <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
