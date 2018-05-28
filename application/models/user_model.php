8508da085108dba8c5680e0d877eeb642d514f43
DESKTOP-Slopsie <slopsie@gmail.com>
2018-05-28 16:58:21 +0200

    Added register function
    You can now register a account wich will show up in the db

---
+++ application/models/user_model.php
@@ -0,0 +1,17 @@
+<?php
+	class user_model extends CI_MODEL{
+		public function register($enc_password){
+			// User data array
+			$data = array(
+				'name' => $this->input->post('name'),
+				'email' => $this->input->post('email'),
+				'username' => $this->input->post('username'),
+				'password' => $enc_password,
+				'zipcode' => $this->input->post('zipcode'),
+			);
+
+			//Insert user
+			return $this->db->insert('users', $data);
+
+		}
+	}
